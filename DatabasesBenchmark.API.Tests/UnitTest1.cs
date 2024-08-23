8eabb32229b3be693be7bcb03f2856137b0d3e8c
gabriel.gimenez <gabogim27@gmail.com>
2024-08-23 12:35:22 +0200

    first commit

---
+++ DatabasesBenchmark.API.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DatabasesBenchmark.API.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
